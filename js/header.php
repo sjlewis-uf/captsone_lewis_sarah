c782f2cc2517765839655836960cf4623df98071
Sarah Lewis <sj.lewis@ufl.edu>
2023-09-10 15:55:40 -0400

    Initial Doc Setup

---
+++ js/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="<?php pageinfo('charset'); ?>">
+        <meta name="viewport" content="width=device=width, initial-scale-1.0"/>
+
+        <title></title>
+
+</head>
